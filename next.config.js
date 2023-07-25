dbe2f2aa712c1419532fdbc7c187702ecd215323
Guilherme Jacob <gjsoaresc@gmail.com>
2023-07-25 20:22:49 -0300

    fix: set output to export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const withNextIntl = require('next-intl/plugin')(
   // This is the default (also the `src` folder is supported out of the box)
-  './src/i18n.ts'
+  './src/i18n.js'
 )
 
 /** @type {import('next').NextConfig} */
-module.exports = withNextIntl({})
+module.exports = withNextIntl({
+  output: 'export',
+})
